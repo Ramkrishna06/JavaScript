c6de604e28433a3e14b06921cd0adca981ee09c7
Ramkrishna06 <ramkrishnaprajapati263@gmail.com>
2026-06-25 14:18:11 +0530

    this is the math class used of random function also with how to fing min and max between two number

--- text.js
+++ text.js
@@ -1,1 +1,43 @@
-console.log("ram");
+// console.log("ram");
+
+
+// let name = 1;
+// console.log(typeof name);
+
+
+const nomodification = function () {
+    console.log("this is our first function");
+}
+
+// data types 
+//primitive
+// String , number , boolean , bigint , null ,undefine  ,symbol 
+// non -primitive data types 
+//object  , arrays , functions , dates.
+
+let name = 'ram'; 
+
+// console.log(`hello ny name is ${name} and i am form now taking 30 days chanllange to chnage myself and it fiunal with`);
+
+const s = new String("hello")
+// console.log(s.charAt(1));
+// console.log(s[1]);
+
+
+
+//some method for better output
+
+const numberband = new Number(10000)
+// console.log(numberband.toString());
+// console.log(numberband.toFixed(2)); //to get 2 number after decimal 
+// console.log(numberband.toLocaleString());
+
+// console.log(String);
+console.log(Math.random()*10 + 1);
+
+const min = 15;
+const max =25;
+
+console.log(Math.floor(Math.random()*10 + 1)+ min);
+
+
